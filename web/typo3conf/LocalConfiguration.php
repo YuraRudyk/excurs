ddd1e4e6e600786b58105423c6602b1437c9b844
Yurii Rudyk <yuriy.rudyk@711media.de>
2017-01-23 16:37:52 +0200

    fixed font roboto

--- web/typo3conf/LocalConfiguration.php
+++ web/typo3conf/LocalConfiguration.php
@@ -3,14 +3,16 @@ return [
     'BE' => [
         'debug' => true,
         'explicitADmode' => 'explicitAllow',
-        'installToolPassword' => '$P$CH78tKwFaVXLWi4/xIbZjlacK73w6j.',
+        'installToolPassword' => '$P$C4vbEZE1m8pjFaXM7.iY90PVyukVG21',
         'loginSecurityLevel' => 'rsa',
     ],
     'EXT' => [
         'extConf' => [
+            'filemetadata' => 'a:0:{}',
             'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
             'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
             'sem_ex_content' => 'a:0:{}',
+            't3editor' => 'a:0:{}',
         ],
     ],
     'FE' => [
@@ -27,7 +29,6 @@ return [
             'TYPO3\CMS\Install\Updates\BackendUserStartModuleUpdate' => 1,
             'TYPO3\CMS\Install\Updates\Compatibility6ExtractionUpdate' => 1,
             'TYPO3\CMS\Install\Updates\ContentTypesToTextMediaUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\ExtensionManagerTables' => 1,
             'TYPO3\CMS\Install\Updates\FileListIsStartModuleUpdate' => 1,
             'TYPO3\CMS\Install\Updates\FilesReplacePermissionUpdate' => 1,
             'TYPO3\CMS\Install\Updates\LanguageIsoCodeUpdate' => 1,
