8b8c5aa3b39307b7efb9a176f783289e6df6d17b
YuraRudyk <yurarudyk@ukr.net>
2017-03-21 21:20:10 +0200

    added the all reviews

--- web/typo3conf/ext/sem_services/Classes/Controller/ServiceController.php
+++ web/typo3conf/ext/sem_services/Classes/Controller/ServiceController.php
@@ -3,7 +3,6 @@
 namespace Rudyk\SemServices\Controller;
 
 use \Rudyk\SemServices\Domain\Repository\ServiceRepository;
-use \Rudyk\SemServices\Domain\Model\Service;
 
 class ServiceController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
@@ -15,14 +14,15 @@ class ServiceController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
     }
 
     /**
-	 * Creates a new review
-	 *
-	 * @param Service $Service 
-	 * @return void
-	 */
-    public function createAction(Service $Service)
-    {
-        var_dump($Service);
+     * Adds a comment to a blog post and redirects to single view
+     *
+     * @param \Rudyk\SemServices\Domain\Model\Service $object
+     * @return void
+     */
+    public function createAction(\Rudyk\SemServices\Domain\Model\Service $object = null )
+    {   
+        $this->objectManager->get(ServiceRepository::class)->add($object);
+        $this->redirect("services");
     }
 
 }
--- web/typo3conf/ext/sem_services/Classes/Domain/Repository/ServiceRepository.php
+++ web/typo3conf/ext/sem_services/Classes/Domain/Repository/ServiceRepository.php
@@ -3,4 +3,15 @@ namespace Rudyk\SemServices\Domain\Repository;
 
 use \TYPO3\CMS\Extbase\Persistence\Repository;
 
-class ServiceRepository extends Repository {}
+class ServiceRepository extends Repository {
+
+	/**
+     * Adds object to queue and persists it to database
+     * @param object $object
+     */
+    public function add($object)
+    {
+        parent::add($object);
+        $this->persistenceManager->persistAll();
+    }
+}
